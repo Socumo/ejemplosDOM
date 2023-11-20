55e3efeb0626082a093e4078405a9db43242ef68
Socumo <alosocumo363@gmail.com>
2023-11-19 22:31:05 -0600

    obtener elementos por medio de sus clases

--- script/scriptPizza.js
+++ script/scriptPizza.js
@@ -3,7 +3,7 @@
 
 const contenedor = document.getElementById('contenedor');
 //console.log(contenedor);
-console.log(contenedor.innerHTML);
+//console.log(contenedor.innerHTML);
 
 /*una de las propiedades es innerHTML nos da acceso a la estructura html
 que esta contenido dentro del elemento contenedor, lo que nos devuelve es una cadena de 
@@ -16,3 +16,10 @@ devuleve un string*/
 const titulo = document.getElementById("titulo");
 console.log(titulo.innerText); //obtengo el texto del titulo.
 console.log(titulo.tagName); //obtengo el nombre de la etiqueta.
+
+/*OBTENER LOS ELEMENTOS POR MEDIO DE SUS CLASES*/
+
+const toppings =  document.getElementsByClassName("topping");
+console.log(toppings); // esto funciona como un arreglo 
+console.log(toppings[0]); // de esta forma puedo mostrar el primer elemento 
+console.log(toppings[0].id); //así puedo obtener el id
