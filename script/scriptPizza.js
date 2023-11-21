f8bbcde55ac31a0c76cc7ea99433a482703631c1
Socumo <alosocumo363@gmail.com>
2023-11-21 14:29:26 -0600

    se agrego un listener para los elementos de la lista de toppings

--- script/scriptPizza.js
+++ script/scriptPizza.js
@@ -1,17 +1,20 @@
-/*RECORRER EL DOM con parentElement(solo elementos de html) o parentNode(incluye comentarios) */
-const listaDeToppings = document.getElementById("lista");
-//console.log(listaDeToppings.parentElement.parentElement);//obtengo el padre del padre de la lista de toppings
-//si quisiera los "hijos" de la lista de toppings:
+const toppings = document.getElementsByClassName("topping");
 
-//console.log(listaDeToppings.children);
-//para obtener el primer "hijo" podemos usar lo siguiete:
-console.log(listaDeToppings.children[0]);
-/*firstChild y lastChild nos obtienen nodos y sería el texto de la indentación 
-pero con firstElementChild y lastElementChild si podemos obtener los datos de HTML*/
 
-console.log(listaDeToppings.firstElementChild);
-console.log(listaDeToppings.lastElementChild);
+function mostrarClic (e){
+    console.log(e.target.innerText);
+}
+for(const topping of toppings){
+    //console.log(topping);
+    topping.addEventListener('click',mostrarClic);
+}
 
-/*PARA OBTENER LOS "HERMANOS" DE UN ELEMENTO*/
-console.log(listaDeToppings.previousElementSibling);// el "hermano" previo a la lista de toppings
-console.log(listaDeToppings.nextElementSibling);// el siguiente hermano de la lista de toppings
+/*otra alternativa si la funcion es pequeña, se puede definir en 
+el propio listener con una funcion flecha:
+
+for(const topping of toppings){
+    topping.addEventListener('click',(e) => {
+    console.log(e.target.innerText);
+});
+}
+*/
