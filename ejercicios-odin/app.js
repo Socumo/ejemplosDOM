9dc45e0a2e7cf9841309d9311e7297650762ff74
Socumo <alosocumo363@gmail.com>
2023-11-22 18:44:04 -0600

    Se creo que elemento titulo y se le cambio el color

--- ejercicios-odin/app.js
+++ ejercicios-odin/app.js
@@ -3,10 +3,16 @@ const contenedor = document.getElementById('container');
 
 //Crear los nodos
 
+    //Nodo parrafo:
 const parrafo = document.createElement('p');
 parrafo.innerText ="Hey I’m red!";
 parrafo.style.color = "red";
 
+    //Nodo titulo h3
+const titulo = document.createElement('h3');
+titulo.innerText = "I’m a blue h3!"
+titulo.style.color = "blue"
 
 //Agregar los elementos al elemento padre
 contenedor.append(parrafo);
+contenedor.append(titulo);
