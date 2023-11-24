4fbcd6ef61973a8871887803b630230df44fcc9a
Socumo <alosocumo363@gmail.com>
2023-11-23 22:07:33 -0600

    Se agregó un div y un titulo a un div creado desde javascript

--- ejercicios-odin/app.js
+++ ejercicios-odin/app.js
@@ -13,6 +13,26 @@ const titulo = document.createElement('h3');
 titulo.innerText = "I’m a blue h3!"
 titulo.style.color = "blue"
 
+    //Div con border negro y de fondo color rosa
+
+const contenedorSecundario = document.createElement('div');
+//set
+contenedorSecundario.style.backgroundColor = 'pink';
+contenedorSecundario.style.border = '5px solid black';
+//contenedorSecundario.style.width = '100px';
+//contenedorSecundario.style.height = '100px';
+
+    //Crear un h1 y un p para ingresarlo en contenedorSecundario
+const tituloh1 = document.createElement("h1");
+tituloh1.innerText = "I’m in a div";
+
+const parrafo2 = document.createElement("p");
+parrafo2.innerText = "Me too!"
+
+contenedorSecundario.append(tituloh1);
+contenedorSecundario.append(parrafo2);
+
 //Agregar los elementos al elemento padre
 contenedor.append(parrafo);
 contenedor.append(titulo);
+contenedor.append(contenedorSecundario);
